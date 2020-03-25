cc5692dbf4952826da30d634eb7621a5dc84fe9c
Ali <ali.nili128@gmail.com>
2020-03-25 10:20:25 -0700

    added login page

---
+++ scripts/login.js
@@ -0,0 +1,52 @@
+let firebaseConfig = {
+  apiKey: "AIzaSyCH6DVqRdYRDyx4s7TrPCSLZ-u6o84L5G8",
+  authDomain: "solar-estimate-6b93d.firebaseapp.com",
+  databaseURL: "https://solar-estimate-6b93d.firebaseio.com",
+  projectId: "solar-estimate-6b93d",
+  storageBucket: "solar-estimate-6b93d.appspot.com",
+  messagingSenderId: "435826401703",
+  appId: "1:435826401703:web:268752d1fa1301011bd682",
+  measurementId: "G-GRPESJ7EDF",
+};
+
+localStorage.setItem("isLoggedIn", false);
+
+firebase.initializeApp(firebaseConfig);
+let db = firebase.firestore();
+let ui = new firebaseui.auth.AuthUI(firebase.auth());
+var uiConfig = {
+  callbacks: {
+    signInSuccessWithAuthResult: function (authResult, redirectUrl) {
+      let user = authResult.user;
+      if (authResult.additionalUserInfo.isNewUser) {
+        db.collection("companies")
+          .doc(user.uid)
+          .set({
+            name: user.displayName,
+            email: user.email,
+            messages: [],
+          })
+          .then(function () {
+            console.log("New user added to firestore");
+            window.location.assign("main.html");
+          })
+          .catch(function (error) {
+            console.log("Error adding new user: " + error);
+          });
+      } else {
+        localStorage.setItem("isLoggedIn", true);
+        localStorage.setItem("name", user.displayName);
+        return true;
+      }
+      return false;
+    },
+  },
+  signInFlow: "popup",
+  signInSuccessUrl: "requests.html",
+  signInOptions: [firebase.auth.EmailAuthProvider.PROVIDER_ID],
+  tosUrl: "main.html",
+  privacyPolicyUrl: "main.html",
+  accountChooserEnabled: false,
+};
+
+ui.start("#firebaseui-auth-container", uiConfig);
