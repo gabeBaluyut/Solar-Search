e0dc86229f1e823bdbb46c39fac2c84806a2a15b
Ali <ali.nili128@gmail.com>
2020-03-25 12:29:10 -0700

    Added read and write from the database for companies.

--- scripts/companies.js
+++ scripts/companies.js
@@ -13,3 +13,36 @@ firebase.initializeApp(firebaseConfig);
 firebase.analytics();
 
 var db = firebase.firestore();
+
+(async () => {
+  let companies = await db.collection("companies").get();
+  companies.forEach((company) => {
+    document.getElementById("companies").innerHTML += `<h3 id=${company.id}>${
+      company.data().name
+    }</h3>`;
+    let companyEl = document.getElementById(company.id);
+    companyEl.style = "cursor: pointer;";
+    console.log("adding event listener");
+    companyEl.onclick = () => {
+      companyEl.style = "solid blue 2px; cursor: pointer;";
+      let message = {
+        bill: localStorage.getItem("bill"),
+        budget: localStorage.getItem("budget"),
+        email: localStorage.getItem("email"),
+        message: localStorage.getItem("message"),
+        name: localStorage.getItem("name"),
+        "roof-pitch": localStorage.getItem("roof-pitch"),
+        zipcode: localStorage.getItem("zipcode"),
+      };
+      db.collection("requests")
+        .add(message)
+        .then((docRef) => {
+          db.collection("companies")
+            .doc(company.id)
+            .update({
+              messages: firebase.firestore.FieldValue.arrayUnion(docRef.id),
+            });
+        });
+    };
+  });
+})();
