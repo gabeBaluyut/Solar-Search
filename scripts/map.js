f552b0b06151f5c605c040009e4da7e508d34d29
Ali <ali.nili128@gmail.com>
2020-03-25 10:22:18 -0700

    improved the map page

--- scripts/map.js
+++ scripts/map.js
@@ -1,30 +1,51 @@
 function initMap() {
-    const BULDING_ZOOM_LEVEL = 20; 
+  const BULDING_ZOOM_LEVEL = 20;
 
-    let map = new google.maps.Map(document.getElementById('map'), {
-        center: {
-            lat: parseInt(localStorage.getItem('lat')),
-            lng: parseInt(localStorage.getItem('lng'))
-        },
-        zoom: BULDING_ZOOM_LEVEL,
-        mapTypeId: 'satellite',
-        tilt: 0
-    });
+  let lat = parseInt(localStorage.getItem("lat"));
+  let lng = parseInt(localStorage.getItem("lng"));
 
-    let drawingManager = new google.maps.drawing.DrawingManager({
-        drawingMode: google.maps.drawing.OverlayType.POLYGON,
-        drawingControl: true,
-        drawingControlOptions: {
-            position: google.maps.ControlPosition.TOP_CENTER,
-            drawingMode: ['polygon']
-        }
-    });
-    drawingManager.setMap(map); 
+  let map = new google.maps.Map(document.getElementById("map"), {
+    center: {
+      lat: 49.337081,
+      lng: -123.168304,
+    },
+    zoom: BULDING_ZOOM_LEVEL,
+    mapTypeId: "satellite",
+    tilt: 0,
+  });
 
-    google.maps.event.addListener(drawingManager,
-        'polygoncomplete', handlePolygonComplete); 
+  let drawingManager = new google.maps.drawing.DrawingManager({
+    drawingMode: google.maps.drawing.OverlayType.POLYGON,
+    drawingControl: true,
+    drawingControlOptions: {
+      position: google.maps.ControlPosition.TOP_CENTER,
+      drawingMode: ["polygon"],
+    },
+  });
+  drawingManager.setMap(map);
+
+  google.maps.event.addListener(
+    drawingManager,
+    "overlaycomplete",
+    handleOverlayComplete
+  );
 }
 
 function handleOverlayComplete(event) {
-    alert('Overlay Complete!');
+  let polygon = event.overlay;
+
+  let area = google.maps.geometry.spherical.computeArea(polygon.getPath());
+  area = Math.floor(area);
+
+  document.getElementById("zas").innerHTML = "Area Selected";
+  document.getElementById("sun-hours").innerHTML = "1214 hours of sunlight";
+  document.getElementById("area").innerHTML =
+    area + " squared meters available for solar panels";
+
+  document.getElementById("buttons").style.visibility = "visible";
 }
+
+document.getElementById("prev").onclick = () =>
+  (window.location = "./bill.html");
+document.getElementById("next").onclick = () =>
+  (window.location = "./result.html");
